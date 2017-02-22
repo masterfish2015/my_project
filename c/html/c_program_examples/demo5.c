fb7a7b38d74d6fda0d0d5d457c11723d06510a19
ldc <925341256@qq.com>
2017-02-22 17:18:13 +0800

    something fix

---
+++ c/html/c_program_examples/demo5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{    
+    printf("%+d\n", 3);
+    printf("%-4d, %4d\n", 3, 3);
+    printf("%04d\n", 3);
+    printf("%#x, %x\n", 30, 30);
+    printf("%#o, %o\n", 30, 30);
+    printf("%#g, %g\n", 3.14, 3.14);
+    printf("%5d, %10f\n", 3, 3.14);
+    printf("%-5s, %-5d\n","abc", 123);
+    printf("%*d\n", 5, 3);
+    printf("%05d, %05d\n", 3, -3);
+    printf("%7.2f, %7.5f\n", 3.1416, 3.1416);
+    printf("%.2g, %.9g\n", 3.1416, 3.1416);
+    printf("%5.2d, %5.2d\n", 3, 1234);
+    printf("%*.*d\n", 5, 3, 3);
+    printf("%5.2s, %5.4s\n", "abc", "ab");
+    return 0;
+}
