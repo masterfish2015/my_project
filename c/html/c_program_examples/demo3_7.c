487e3c2aef528afd98b954880ae71bb4fcefc1ab
masterfish2015 <925341256@qq.com>
2018-10-04 23:22:02 +0800

    add new

--- c/html/c_program_examples/demo3_7.c
+++ c/html/c_program_examples/demo3_7.c
@@ -2,7 +2,10 @@
 
 int main()
 {
-  float f = 123456789;
-  printf("%e\n",f);
+  float a=3.14;
+  double b=0.00123456789;
+  printf("0123456789\t0123456789\t0123456789\n");
+  printf("%f\t%10.2f\t%-10.2f\n",a,a,a);
+  printf("%le\t%10.2le\t%.2le\n",b,b,b);
   return 0;
 }
