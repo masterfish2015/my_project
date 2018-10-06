a0974036d215efe2eebb1b4edd43b9d266a83367
masterfish2015 <925341256@qq.com>
2018-10-06 20:20:49 +0800

    finish c chapter3

--- c/html/c_program_examples/demo3_7.c
+++ c/html/c_program_examples/demo3_7.c
@@ -1,11 +1,8 @@
 #include "stdio.h"
-
 int main()
 {
-  float a=3.14;
-  double b=0.00123456789;
-  printf("0123456789\t0123456789\t0123456789\n");
-  printf("%f\t%10.2f\t%-10.2f\n",a,a,a);
-  printf("%le\t%10.2le\t%.2le\n",b,b,b);
+  int k = 011;
+  printf("%d,", k++);
+  printf("%d\n",k);
   return 0;
 }
